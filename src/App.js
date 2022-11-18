09736c808daa90e880655e6c08e0d78eb0916af6
Marlon Baldovino <37963450+sharkwhite21@users.noreply.github.com>
2022-11-18 15:30:48 -0500

    update/ Aplicacion de las render props

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import { ListOfCategories } from './components/ListOfCategorys.js'
 import { GlobalStyle } from './styles/GlobalStyles.js'
 import { ListOfPhotocards } from './container/ListOfPhotoCards'
 import { Logo } from './components/Logo/index.js'
+import { PhotoCardWithQuery } from './container/PhotoCardWithQuery.js'
 
 export const App = () => {
   const urlParams = new window.URLSearchParams(window.location.search)
   const detailID = urlParams.get('detail')
+  console.log(detailID)
   return (
    <>
      <GlobalStyle />
      <Logo />
      {
-       detailID ? <h1> Detail Id</h1> : <Fragment>
+       detailID ? <PhotoCardWithQuery id={detailID} /> : <Fragment>
          <ListOfCategories />
          <ListOfPhotocards categoryId={1} />
        </Fragment>
